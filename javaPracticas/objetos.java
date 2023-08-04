95b56948006d510bcac7e661d516ff41209c004b
CrisHzz <124224845+CrisHzz@users.noreply.github.com>
2023-08-04 03:33:50 +0000

    Codigo bueno y documentado V2

--- javaPracticas/objetos.java
+++ javaPracticas/objetos.java
@@ -1,41 +1,35 @@
+
 public class objetos{
-    public static void main(String[] args) { //Clase main
-        doctor firstDoctor =new doctor();
-        firstDoctor.setname("Neider Machado"); //Cree un objeto y le puse el nombre.
-        firstDoctor.setid("47");
-        firstDoctor.setspeciality("Surgeon");
-        firstDoctor.show_name(); //Cree un metodo para mostrar el nombre
-        firstDoctor.show_id();
-        firstDoctor.show_speciality();       
-        
-    }
+   public static void main(String[] args) { //Clase main
+      doctor firstDoctor =new doctor(); //Creo el objeto doctor first doctor
+      firstDoctor.name="Neider galvis"; //Le asigno un nombre
+      firstDoctor.show_name(); //Metodo para mostrar el nombre
+      firstDoctor.show_id();
+      firstDoctor.speciality=("Surgeon");
+      firstDoctor.show_speciality();
+            
+   }
 }
 
- class doctor{
-    String id;
-    String name;
-    String speciality;
-
- public void setname(String name){ //Asignar atributo a funcion
-   this.name=name;
- }
- public void setid(String id){
-   this.id=id;
- }
-
- public void setspeciality(String speciality){
-   this.speciality=speciality;
- }
-
- public void show_name(){
-    System.out.print(name);
- }
- public void show_id(){
-    System.out.println(id);
- }
- public void show_speciality(){
-    System.out.print(speciality);
- }
-
- }   
+class doctor{
+   static int id=0; //Lo pongo estatico porque quiero que se lo inacializo en 0
+   String name;
+   String speciality;
+   
+   doctor(){
+      System.out.println("Se esta construyendo el doctor"); //Cada vez que se cree un doctor le pone un +1
+      id++;
+   } 
+      
+   
+   public void show_name(){
+      System.out.println("Bienvenido doctor"+""+name); //metodo
+   }
+   public void show_id(){
+     System.out.println("Doctor N°"+""+id);   
+   }
+   public void show_speciality(){
+      System.out.println("Tiene un especialista en "+""+speciality);
+   }
+}
 
