886d28bfd3a7e39319a719322d3f559faf526fa7
CrisHzz <124224845+CrisHzz@users.noreply.github.com>
2023-08-09 03:11:57 +0000

    Get and set

--- javaPracticas/main.java
+++ javaPracticas/main.java
@@ -18,8 +18,59 @@ public class main {
             this.grupo = grupo;
         }
 
+        
+
+        public int getId() {
+            return id;
+        }
+
+
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+
+
+        public String getNombre() {
+            return nombre;
+        }
+
+
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+
+
+        public String getApellido() {
+            return apellido;
+        }
+
+
+
+        public void setApellido(String apellido) {
+            this.apellido = apellido;
+        }
+
+
+
+        public String getGrupo() {
+            return grupo;
+        }
+
+
+
+        public void setGrupo(String grupo) {
+            this.grupo = grupo;
+        }
+
+
+
         public void mostrarNombre(){
             System.out.println("Hola, mi nombre es " + nombre);
         }
     }
+
 }
