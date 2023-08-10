8adf40b35fb799c9a4f817696efa00efa02145e7
CrisHzz <124224845+CrisHzz@users.noreply.github.com>
2023-08-10 04:14:19 +0000

    jefe

--- javaPracticas/Consultor.java
+++ javaPracticas/Consultor.java
@@ -1,6 +1,30 @@
 public class Consultor extends Persona{
-    
-    
+    String nombre_consultor;
+    int numero_consultor;
 
-   
+    public Consultor(int id, String Cc, String nombre, String apellido, String domicilio, String telefono,String nombre_consultor,int numero_consultor){
+        super(id, Cc, nombre_consultor, apellido, domicilio, telefono);
+        this.nombre_consultor=nombre_consultor;
+        this.numero_consultor=numero_consultor;
+        
+    }
+
+    public String getNombre_consultor() {
+        return nombre_consultor;
+    }
+
+    public void setNombre_consultor(String nombre_consultor) {
+        this.nombre_consultor = nombre_consultor;
+    }
+
+    public int getNumero_consultor() {
+        return numero_consultor;
+    }
+
+    public void setNumero_consultor(int numero_consultor) {
+        this.numero_consultor = numero_consultor;
+    }
+
+
+
 }
